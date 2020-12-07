0fa0333322a39335dfc096f2e4b6e2fc63114be4
GShacklebolt <ghazali123456@gmail.com>
2020-12-07 19:45:49 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Hi there! 👋
+# This project is named **Web Diary** 
+
+***
+
+The basic idea behind this project is to create an online diary that logs in your daily diary entries.
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Diary</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+<div id="particles-js">
+	<div class="diary">
+		<h1>Ghazali's Diary</h1>
+	</div>
+</div>
+
+<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script type="text/javascript" src="./scripts/particleStyle.js"></script>
+</body>
+</html>
